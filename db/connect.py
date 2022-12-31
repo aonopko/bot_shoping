e70391e9c5f18e7e9089b15790fd5951b6af17a9
aonopko <aonopko.rpu@gmail.com>
2022-12-31 05:40:19 -0500

    recconect

--- db/connect.py
+++ db/connect.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = ""
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:1983@localhost/adpanel"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
