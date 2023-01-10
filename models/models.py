e526b8bb87fa45247becda14d2bae250fd3412c1
aonopko <aonopko.rpu@gmail.com>
2023-01-10 16:16:52 -0500

    add sql models

--- models/models.py
+++ models/models.py
@@ -1,26 +1,24 @@
 
-from sqlalchemy import Integer, String, Column, DateTime, ForeignKey, sql
+from sqlalchemy import Integer, String, Column, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
-from db.core import db_gino
+from db.base import Base
 
 
-class Admin(db_gino.Model):
+class Admin(Base):
     __tablename__ = "administrators"
-    query: sql.Select
-
     id = Column(Integer, primary_key=True, index=True, unique=True)
     date = Column(DateTime)
-    employee = Column(Integer, ForeignKey("employee.id"))
-    id_employee = relationship("Employee")
+    employee_id = Column(ForeignKey("employee.id"))
 
 
-class Employee(db_gino.Model):
+class Employee(Base):
     __tablename__ = "employee"
-    id = Column(Integer, primary_key=True, unique=True)
+    id = Column(Integer, ForeignKey("admin.id"), primary_key=True, unique=True)
     name = Column(String(255))
     surname = Column(String(255))
     phone = Column(Integer)
     date = Column(DateTime)
+
 
 
 
