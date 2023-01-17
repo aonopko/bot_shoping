f2b701b6c5e942fb0ff34369d356aaf576043496
aonopko <aonopko.rpu@gmail.com>
2023-01-17 14:27:28 -0500

    add relationship models

--- models/db_models.py
+++ models/db_models.py
@@ -5,16 +5,16 @@ from db.base import db_gino
 
 
 class Customer(db_gino.Model):
-    __tablename__ = "customer"
+    __tablename__ = "customers"
     id_telegram = Column(Integer, unique=True, primary_key=True)
-    name = Column(String(255))
+    name = Column(String(255), nullable=True)
     phone = Column(Integer)
     email = Column(String(255))
 
 
 class Product(db_gino.Model):
-    __tablename__ = "product"
-    product_id = Column(Integer, unique=True, primary_key=True)
+    __tablename__ = "products"
+    id = Column(Integer, unique=True, primary_key=True)
     name = Column(String(50))
     category = Column(String(50))
     sub_category = Column(String(50))
@@ -23,15 +23,15 @@ class Product(db_gino.Model):
     photo = Column(String(250))
 
 
-class Orders(db_gino.Model):
+class Order(db_gino.Model):
     __tablename__ = "orders"
     order_id = Column(Integer, unique=True, primary_key=True)
-    customer_id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey("customers.id_telegram"))
     order_date = Column(DateTime)
 
 
 class OrderProducts(db_gino.Model):
     __tablename__ = "order_products"
-    order_id = Column(Integer, primary_key=True)
-    product_id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey("orders.id"), ondelete="CASCADE")
+    product_id = Column(Integer, ForeignKey("products.id", ondelete="CASCADE"))
     quantity = Column(Integer)
