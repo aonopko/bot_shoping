fbbc6b680071c746497e1a85b05b3c4a3f574b83
aonopko <aonopko.rpu@gmail.com>
2023-01-18 12:07:06 -0500

    finishd add models

--- models/db_models.py
+++ models/db_models.py
@@ -6,15 +6,15 @@ from db.base import db_gino
 
 class Customer(db_gino.Model):
     __tablename__ = "customers"
-    id_telegram = Column(Integer, unique=True, primary_key=True)
-    name = Column(String(255), nullable=True)
-    phone = Column(Integer)
-    email = Column(String(255))
+    id_telegram = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    phone = Column(Integer, unique=True)
+    email = Column(String(255), unique=True)
 
 
 class Product(db_gino.Model):
     __tablename__ = "products"
-    id = Column(Integer, unique=True, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(50))
     category = Column(String(50))
     sub_category = Column(String(50))
@@ -25,13 +25,13 @@ class Product(db_gino.Model):
 
 class Order(db_gino.Model):
     __tablename__ = "orders"
-    order_id = Column(Integer, unique=True, primary_key=True)
+    id = Column(Integer, primary_key=True)
     customer_id = Column(Integer, ForeignKey("customers.id_telegram"))
     order_date = Column(DateTime)
 
 
-class OrderProducts(db_gino.Model):
+class OrderProduct(db_gino.Model):
     __tablename__ = "order_products"
-    order_id = Column(Integer, ForeignKey("orders.id"), ondelete="CASCADE")
-    product_id = Column(Integer, ForeignKey("products.id", ondelete="CASCADE"))
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
     quantity = Column(Integer)
