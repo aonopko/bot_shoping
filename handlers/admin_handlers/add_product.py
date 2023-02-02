493777340d7284a91c98b9e82c48d1c5e5425906
aonopko <aonopko.rpu@gmail.com>
2023-02-02 11:10:50 -0500

    State admin

--- handlers/admin_handlers/add_product.py
+++ handlers/admin_handlers/add_product.py
@@ -5,6 +5,10 @@ from aiogram.dispatcher import FSMContext
 from states.admin_state import AddProduct
 
 
-async def add_id(m: Message, state: FSMContext):
+async def add_id(m: Message):
     await AddProduct.id_product.set()
     await m.answer("Додайте id товару", m.from_user.id)
+
+async def load_id(m: Message, state: FSMContext):
+    async with state.proxy() as data:
+        pass
