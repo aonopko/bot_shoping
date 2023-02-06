0ebc0149a4c4545df53481feb005f2753b409163
aonopko <aonopko.rpu@gmail.com>
2023-02-06 07:11:02 -0500

    add inline kb

--- handlers/admin_handlers/add_product.py
+++ handlers/admin_handlers/add_product.py
@@ -11,7 +11,7 @@ from keyboards.inline.admin_kb import add_product
 
 async def add_id(m: Message):
     await AddProduct.id_product.set()
-    await m.answer("Додайте id товару")
+    await m.answer("\U0000231B Додайте id товару")
 
 
 async def load_id(m: Message, state: FSMContext):
@@ -19,35 +19,43 @@ async def load_id(m: Message, state: FSMContext):
         try:
             data["id_product"] = int(m.text)
         except ValueError:
-            await m.answer("\U0001F69A  Потрібно ввести число")
+            await m.answer("\U0000203C  Потрібно ввести число")
         else:
             await AddProduct.next()
-            await m.answer("id завантажено")
-            await m.answer("Додайте назву товару")
+            await m.answer("\U0000231B Додайте назву товару")
 
 
 async def load_name(m: Message, state: FSMContext):
     async with state.proxy() as data:
-        data["name"] = m.text
-    await m.answer("Назву додано")
-    await AddProduct.next()
-    await m.answer("Додайте категорию товару")
+        try:
+            data["name"] = str(m.text)
+        except ValueError:
+            await m.answer("\U0000203C Потрібно ввести буквами")
+        else:
+            await AddProduct.next()
+            await m.answer("\U0000231B Додайте категорию товару")
 
 
 async def load_category(m: Message, state: FSMContext):
     async with state.proxy() as data:
-        data["category"] = m.text
-        await m.answer("Категорію додано")
-        await AddProduct.next()
-        await m.answer("Додайте підкатегорію")
+        try:
+            data["category"] = m.text
+        except ValueError:
+            await m.answer("\U0000203C Потрібно ввести буквами")
+        else:
+            await AddProduct.next()
+            await m.answer("\U0000231B Додайте підкатегорію")
 
 
 async def load_sub_category(m: Message, state: FSMContext):
     async with state.proxy() as data:
-        data["sub_category"] = m.text
-        await m.answer("ПідКатегорію додано")
-        await AddProduct.next()
-        await m.answer("Додайте ціну товара")
+        try:
+            data["sub_category"] = str(m.text)
+        except ValueError:
+            await m.answer("\U0000203C Потрібно ввести буквами")
+        else:
+            await AddProduct.next()
+            await m.answer("\U0000231B Додайте ціну товара")
 
 
 async def load_price(m: Message, state: FSMContext):
@@ -57,9 +65,8 @@ async def load_price(m: Message, state: FSMContext):
         except ValueError:
             await m.answer("Потрібно ввести число")
         else:
-            await m.answer("Ціну додано")
             await AddProduct.next()
-            await m.answer("Додайте кількість товару")
+            await m.answer("\U0000231B Додайте кількість товару")
 
 
 async def load_quantity(m: Message, state: FSMContext):
@@ -69,23 +76,32 @@ async def load_quantity(m: Message, state: FSMContext):
         except ValueError:
             await m.answer("Потрібно ввести число")
         else:
-            await m.answer("Кількість додано")
             await AddProduct.next()
-            await m.answer("Додайте фото товру")
+            await m.answer("\U0000231B Додайте фото товру")
 
 
 async def load_photo(m: Message, state: FSMContext):
     async with state.proxy() as data:
         data["photo"] = m.photo[0].file_id
-        await m.answer("фото додано", )
         await AddProduct.next()
         await m.answer("НАТИСНИТЬ ЩО НЕБУДЬ", reply_markup=add_product)
 
 
-async def test(call: CallbackQuery, state: FSMContext):
-    await call.message.answer("Нажали єту кнопку")
+async def inline_del(call: CallbackQuery, state: FSMContext):
+    await call.message.answer("Додайте товар спочатку")
     await call.answer()
     await state.finish()
+
+
+
+# async def inline_add(call: CallbackQuery, state: FSMContext):
+#     await AddProduct.add_prod.set()
+#     await call.message.answer("Нажали єту кнопку")
+#     await call.answer()
+#     await state.finish()
+
+
+
 
 
 def register_add_product_handlers(dp: Dispatcher):
@@ -105,5 +121,9 @@ def register_add_product_handlers(dp: Dispatcher):
                                 state=AddProduct.quantity)
     dp.register_message_handler(load_photo, content_types=["photo"],
                                 state=AddProduct.photo)
-    dp.register_callback_query_handler(test, add_callback.filter(add_prod="add_item"),
-                                       state=AddProduct.add_prod)
+    # dp.register_callback_query_handler(inline_add,
+    #                                    add_callback.filter(add_prod="add_item"),
+    #                                    state=AddProduct.add_prod)
+    dp.register_callback_query_handler(inline_del,
+                                       delete_callback.filter(del_prod="del_item"),
+                                       state=AddProduct.del_prod)
